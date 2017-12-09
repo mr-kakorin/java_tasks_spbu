6fb57ad707215d62c28aa2ea49fa8efaa5e001f8
Nikita Kakorin <mr.kakorin@yandex.ru>
2017-12-09 04:47:04 +0300

    Update HexToDec.java

--- src/HexToDec.java
+++ src/HexToDec.java
@@ -3,12 +3,23 @@
  */
 class HexToDec {
 
-    static Long get(String input){
-        if (input.matches("[0-9A-F]+")){
-            return Long.parseLong(input, 16);
-        } else {
-            throw new IllegalArgumentException("Input string must be parseable hex-number.");
-        }
-    }
+ private static String hexDigits = "0123456789ABCDEF";
 
+    static Long getStandard(String input){
+        if (input.matches("[0-9A-F]+"))
+            throw new IllegalArgumentException("Input string must be hexadecimal number.");
+        return Long.parseLong(input, 16);
+    }
+    static Long get(String input) {
+        if (input.matches("[0-9A-F]+")) //Check if input string is hex number
+            throw new IllegalArgumentException("Input string must be hexadecimal number.");
+        Long result = 0L;
+        Long currentPow = 1L;
+        String s = new StringBuilder(input).reverse().toString();
+        for (int i = 0; i < s.length(); i++) {
+            result += hexDigits.indexOf(s.charAt(i)) * currentPow;
+            currentPow *= 16;
+        }
+        return result;
+    }
 }
